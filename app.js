c5980b8b5fe47d8aba35c69cacbd3f2baab13891
Lucas Voltera <lucasvoltera@gmail.com>
2024-11-03 11:10:28 -0300

    feat: add render function

---
+++ app.js
@@ -0,0 +1,4 @@
+
+let message = 'Hello world!!';
+
+render('#container', `<h1>${message}</h1>`);
---
+++ jiggle.js
@@ -0,0 +1,10 @@
+
+
+function render(element, content) {
+    const app = document.querySelector(element);
+
+    if (app !== null) {
+        app.innerHTML = content;
+    }
+
+}
