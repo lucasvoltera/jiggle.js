6310ffb986acd1a8622eda20cf9c29928a7131e9
Lucas Voltera <lucasvoltera@gmail.com>
2024-11-03 12:03:38 -0300

    feat: testing changes functions

--- app.js
+++ app.js
@@ -1,13 +1,68 @@
+const mockWeatherData = {
+    "New York": {
+        temperature: 300,
+        humidity: 50,
+        description: "Sunny",
+    },
+
+    "London": {
+        temperature: 290,
+        humidity: 70,
+        description: "Rainy",
+    },
+
+    "Tokyo": {
+        temperature: 310,
+        humidity: 80,
+        description: "Cloudy",
+    },
+
+}
 
 
-let state = reactive({message: 'hello world!'});
+let state = reactive({
+    selectedCity: 'London',
+    weather: {
+        temperature: 'N/A',
+        humidity: 'N/A',
+        description: 'N/A',
+    },
+});
+
+function updateWeather(city) {
+    const weatherData = mockWeatherData[city];
+    state.weather = weatherData;
+    state.selectedCity = city;
+    // renderApp();
+}
+
+async function fetchWeather(city) {
+    await updateWeather(city);
+}
 
 
 function renderApp() {
-    render('#container', `<h1>${state.message}</h1>`);
+    render('#container', `<select onChange=updateSelectedCity(this.value)>
+            <option value="Tokyo">Tokyo</option>
+            <option value="London">London</option>
+            <option value="New York">New York</option>
+        
+        </select>
+        <div>
+            <p>Temperature: ${state.weather.temperature}</p>
+            <p>Humidity: ${state.weather.humidity}</p>
+            <p>Description: ${state.weather.description}</p>
+
+        </div>
+        `);
 }
 
 renderApp()
+
+function updateSelectedCity(city) {
+    state.selectedCity = city
+    fetchWeather(city)
+}
 
 const timeout = 2000; // 2 seconds
 setTimeout(() => {
---
+++ data.js
@@ -0,0 +1,22 @@
+const mockWeatherData = {
+    "New York": {
+        temperature: 300,
+        humidity: 50,
+        description: "Sunny",
+    },
+
+    "London": {
+        temperature: 290,
+        humidity: 70,
+        description: "Rainy",
+    },
+
+    "Tokyo": {
+        temperature: 310,
+        humidity: 80,
+        description: "Cloudy",
+    },
+
+}
+
+export default mockWeatherData;
