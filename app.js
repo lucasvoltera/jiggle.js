7ec0362d2b12da6a0510b3d67b14bf4d7815c979
Lucas Voltera <lucasvoltera@gmail.com>
2024-11-03 11:24:28 -0300

    feat: add state changing function

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 
-let message = 'Hello world!!';
 
-render('#container', `<h1>${message}</h1>`);
+let state = reactive({message: 'hello world!'});
+
+
+function renderApp() {
+    render('#container', `<h1>${state.message}</h1>`);
+}
+
+renderApp()
+
+const timeout = 2000; // 2 seconds
+setTimeout(() => {
+    state.message = 'Hello, universe!'
+}, timeout);
--- jiggle.js
+++ jiggle.js
@@ -8,3 +8,33 @@ function render(element, content) {
     }
 
 }
+
+
+function reactive(obj) {
+
+    const keys = Object.keys(obj)
+
+    const reactiveObj = {}
+
+    keys.forEach((key) => {
+        let value = obj[key]
+
+        
+        Object.defineProperty(reactiveObj, key, {
+            get() {
+                console.log('get', key, value)
+                return value
+            },
+            set(newValue) {
+                console.log('set', key, newValue)
+                value = newValue
+                renderApp()                
+            }
+        })
+    }
+)
+
+    
+
+    return reactiveObj
+}
